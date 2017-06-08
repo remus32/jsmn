716b5f27d919d7a6cf55b1c372cdaea6b6aaa0a5
Remus32 <matyas.bobek@gmail.com>
2017-06-08 06:57:15 +0200

    Add parse function

--- native.c
+++ native.c
@@ -1,5 +1,29 @@
 #include "jsmn.h"
 #include <stdint.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define MEM_START 32
+#define MEM_MULTI 2
+
+jsmntok_t *native_parse(const char *str){
+    jsmn_parser parser;
+    jsmn_init(&parser);
+    uint32_t count = MEM_START;
+    int r = JSMN_ERROR_NOMEM;
+    jsmntok_t *tok = NULL;
+    while(r = JSMN_ERROR_NOMEM){
+        if(tok != NULL)free(tok);
+        tok = malloc(sizeof(jsmntok_t) * count);
+        r = jsmn_parse(&parser, str, strlen(str), tok, count);
+        count *= MEM_MULTI;
+    }
+    if(r >= 0){
+        return tok;
+    }else{
+        return NULL;
+    }
+}
 
 uint32_t json_primitive() { return JSMN_PRIMITIVE; }
 uint32_t json_object() { return JSMN_OBJECT; }
